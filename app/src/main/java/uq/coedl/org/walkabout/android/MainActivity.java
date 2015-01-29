2eb93a2ddb710d0544a10ca2a3d6873cd4c94bf2
mplanigale <lists.mark@planigale.com>
2015-01-29 22:02:05 +1100

    Add location polling functionality to MainActivity

--- app/src/main/java/uq/coedl/org/walkabout/android/MainActivity.java
+++ app/src/main/java/uq/coedl/org/walkabout/android/MainActivity.java
@@ -1,11 +1,15 @@
 package uq.coedl.org.walkabout.android;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import uq.coedl.org.walkabout.GameManager;
+import uq.coedl.org.walkabout.LocationInterface;
 import uq.coedl.org.walkabout.R;
 
 
@@ -13,15 +17,19 @@ public class MainActivity extends ActionBarActivity
 {
     private static GameManager gameManager = new GameManager();
 
+    private LocationManager locationManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //initialise the location manager to allow GPS polling
+        initialiseLocationManager();
+
         if(!gameManager.isInitialised())
         {
-
             gameManager.initialise(null, null);
         }
     }
@@ -50,4 +58,29 @@ public class MainActivity extends ActionBarActivity
 
         return super.onOptionsItemSelected(item);
     }
+
+    /**
+     * Acquire a reference to the system Location Manager
+     */
+    private void initialiseLocationManager() {
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+    }
+
+    /**
+     * Get coordinates of the last known GPS location of the device
+     * @return object containing coordinates
+     */
+    public LocationInterface getLastKnownLocation() {
+        //first check that the location manager is initialised
+        if (locationManager == null) {
+            initialiseLocationManager();
+        }
+
+        //now poll it to get GPS coordinates
+        Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+        //wrap in LocationAndroid object and return
+        return new LocationAndroid(lastKnownLocation);
+    }
+
 }
